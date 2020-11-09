3a6cdf149129036d365847327f6d64808a063fea
leshik-kl <71494229+leshik-kl@users.noreply.github.com>
2020-11-09 18:38:43 +0700

    Add files via upload

---
+++ lr4.cpp
@@ -0,0 +1,17 @@
+/*вычесление чисел*/
+
+#include <iostream>
+
+using namespace std;
+
+main() {
+	int count = 0;
+	
+	while(count < 101) {
+		cout << count << " ";
+		count++;
+		}
+	cin.get();
+	cin.get();
+
+	}
